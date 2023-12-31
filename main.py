3f46e5beb9d29c7f4efde4a8e90c5b8ef1606566
aloneworker <aloneworker@gmail.com>
2023-12-31 18:28:16 +0800

    Update main.py
    degut

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def load_names(filename):
         return [line.strip() for line in file]
 
 class Girl:
-    def __init__(self, name, features):
+    def __init__(self, name="", features=[]):
         self.name = name
         self.features = features
         self.moods = ["閒聊", "無聊", "認真"]
@@ -44,7 +44,7 @@ class Player:
     def cast_summon_girl(self):
         if self.mana >= 10:
             self.mana -= 10
-            return Girl()
+            return Girl()  # 在這裡不提供名字和特徵
         else:
             return None
 
@@ -56,36 +56,24 @@ def main():
     girl = None
     girl_names = load_names("girl_names.txt")
     possible_features = ["巨乳", "翹臀", "長腿", "正妹", "蛇腰"]
+    
     while True:
-        print(f"\n當前咒力：{player.get_mana()}")
-        if not girl:
-            print("1. 招喚妹子術")
-        else:
-            print("2. 與妹子閒聊")
-            print("3. 與妹子認真聊天")
-        print("4. 結束遊戲")
-
-        choice = input("請選擇你的行動：")
-        if choice == '1' and not girl:
-            girl = player.cast_summon_girl()
-            if girl:
-                print("你成功招喚了一位妹子！")
-            else:
-                print("咒力不足！")
-        elif choice in ['2', '3'] and girl:
-            mood = "閒聊" if choice == '2' else "認真"
-            print(girl.chat(mood))
-        leave_message = girl.leave_check()
-        if leave_message and "是否要加入本子" in leave_message:
-            choice = input("你同意嗎？(yes/no): ")
-            if choice.lower() == 'yes':
-                name = random.choice(girl_names)
-                features = random.sample(possible_features, random.randint(0, 3))
-                girl = Girl(name, features)
-                print(f"妹子{name}加入了你的本子，她的特徵是：{', '.join(features)}")
-                girl = None
-            else:
-                print("妹子離開了。")
+        # 原始代碼保持不變，直到...
+        if girl:
+            leave_message = girl.leave_check()
+            if leave_message and "是否要加入本子" in leave_message:
+                choice = input("你同意嗎？(yes/no): ")
+                if choice.lower() == 'yes':
+                    name = random.choice(girl_names)
+                    features = random.sample(possible_features, random.randint(0, 3))
+                    girl = Girl(name, features)
+                    print(f"妹子{name}加入了你的本子，她的特徵是：{', '.join(features)}")
+                    girl = None
+                else:
+                    print("妹子離開了。")
+                    girl = None
+            elif leave_message:
+                print(leave_message)
                 girl = None
         elif leave_message:
             print(leave_message)
