c0dc70625b09737d31633bf09229c7a6475d0b8e
aloneworker <aloneworker@gmail.com>
2023-12-31 16:42:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ class Player:
     def __init__(self):
         self.mana = 100  # 初始咒力
 
-    def cast_spell(self):
-        if self.mana >= 10:
-            self.mana -= 10
-            print("你發動了一個強大的咒術！")
+    def cast_spell(self, spell):
+        if self.mana >= 20:
+            self.mana -= 20
+            if spell == "summon":
+                print("你成功招喚了一位妹子！")
+            elif spell == "manga":
+                print("一堆新鮮的本子出現了！")
         else:
             print("咒力不足！")
 
@@ -20,16 +23,19 @@ def main():
     player = Player()
     while True:
         print(f"\n當前咒力：{player.get_mana()}")
-        print("1. 發動咒術")
-        print("2. 充能咒力")
-        print("3. 結束遊戲")
+        print("1. 招喚妹子術")
+        print("2. 本子術")
+        print("3. 充能咒力")
+        print("4. 結束遊戲")
         choice = input("請選擇你的行動：")
 
         if choice == '1':
-            player.cast_spell()
+            player.cast_spell("summon")
         elif choice == '2':
-            player.recharge()
+            player.cast_spell("manga")
         elif choice == '3':
+            player.recharge()
+        elif choice == '4':
             print("遊戲結束。")
             break
         else:
