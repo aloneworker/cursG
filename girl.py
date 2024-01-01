1d425709704fbf8b316f818e92b4054d88c007c5
aloneworker <aloneworker@gmail.com>
2024-01-01 15:06:06 +0800

    Update girl.py

--- girl.py
+++ girl.py
@@ -13,7 +13,7 @@ class Girl:
         excitement (int): 妹子的興奮程度。
         relationship (int): 與玩家的關係值。
         action (str): 妹子當前的行動。
-        male_friends (list): 妹子本子中的男性朋友列表。
+        male_friends (list): 儲存妹子本子中的男子列表。
     """
     
     RELATIONSHIP_STAGES = ["陌生", "朋友", "情侶", "夫妻"]
@@ -33,7 +33,8 @@ class Girl:
 
     def update_action(self):
         """
-        更新妹子的行動，如果有男性朋友，則增加與男性朋友互動的行動選項。
+        更新妹子的行動。如果有男性朋友，則增加與男性朋友互動的行動選項。
+        如果妹子的當前行動是被搭訕，則有1/3的機會認識男子，並將他加入到本子中。
         """
         if self.male_friends:
             all_actions = self.ACTIONS + self.MALE_ACTIONS
@@ -50,7 +51,7 @@ class Girl:
         根據關係值更新與玩家的關係階段。
 
         返回:
-            str: 當前關係階段。
+            str: 當前的關係階段。
         """
         if self.relationship > 6:
             return self.RELATIONSHIP_STAGES[3]  # 夫妻
@@ -61,4 +62,17 @@ class Girl:
         else:
             return self.RELATIONSHIP_STAGES[0]  # 陌生
 
-    # 聊天和離開檢查方法保持不變
+    def chat(self, choice):
+        """
+        與妹子進行聊天。根據玩家選擇的話題和妹子的當前心情，決定聊天的結果。
+
+        參數:
+            choice (str): 玩家選擇的聊天話題。
+
+        返回:
+            tuple: 包含聊天結果的字符串和一個布爾值，指示妹子是否離開。
+        """
+        if choice == self.current_mood:
+            result = random.choices(["高興", "厭煩", "無感"], weights=[50, 30, 20])[0]
+        else:
+            result = random.choices(["高興", "厭煩", "無感"], weights=[30
