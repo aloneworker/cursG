8c4375152b4688919417bb370a479f03f05a18c8
aloneworker <aloneworker@gmail.com>
2024-01-01 14:57:40 +0800

    分隔

--- main.py
+++ main.py
@@ -1,87 +1,10 @@
+from girl import Girl
+from player import Player
 import random
 
 def load_names(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return [line.strip() for line in file]
-
-class Girl:
-    RELATIONSHIP_STAGES = ["陌生", "朋友", "情侶", "夫妻"]
-
-    def __init__(self, name="", features=[]):
-        self.name = name
-        self.features = features
-        self.moods = ["閒聊", "無聊", "認真"]
-        self.current_mood = random.choice(self.moods)
-        self.displeasure = 0
-        self.excitement = 0
-        self.relationship = 0
-
-    def update_relationship(self):
-        if self.relationship > 6:
-            return self.RELATIONSHIP_STAGES[3]  # 夫妻
-        elif self.relationship > 4:
-            return self.RELATIONSHIP_STAGES[2]  # 情侶
-        elif self.relationship > 2:
-            return self.RELATIONSHIP_STAGES[1]  # 朋友
-        else:
-            return self.RELATIONSHIP_STAGES[0]  # 陌生
-
-    def chat(self, choice):
-        if choice == self.current_mood:
-            result = random.choices(["高興", "厭煩", "無感"], weights=[50, 30, 20])[0]
-        else:
-            result = random.choices(["高興", "厭煩", "無感"], weights=[30, 50, 20])[0]
-
-        if result == "高興":
-            self.excitement += 1
-            if self.excitement > 3:  # 當高興超過3次，關係增加
-                self.relationship += 1
-                relationship_stage = self.update_relationship()
-                return f"妹子感到高興了！關係增加，現在是{relationship_stage}。妹子離開了。", True
-            return "妹子感到高興了！", False
-        elif result == "厭煩":
-            self.displeasure += 1
-            if self.displeasure > 5:  # 當厭煩超過5次，妹子離開
-                return "妹子感到厭煩了。妹子離開了。", True
-            return "妹子感到厭煩了。", False
-        else:
-            return "妹子無感。", False
-
-    def leave_check(self):
-        if self.displeasure > 5:
-            return "妹子因為反感太多而離開了。"
-        elif self.excitement > 5:
-            return "妹子很興奮，問你是否要加入本子。"
-        return None
-
-class Player:
-    def __init__(self):
-        self.mana = 100
-        self.book = []  # 存儲本子中的妹子
-
-    def cast_summon_girl(self):
-        if self.mana >= 10:
-            self.mana -= 10
-            return Girl()
-        else:
-            return None
-
-    def add_girl_to_book(self, girl):
-        self.book.append(girl)
-
-    def cast_book_spell(self):
-        if self.mana >= 3 and self.book:
-            self.mana -= 3
-            print("本子中的妹子：")
-            for index, girl in enumerate(self.book):
-                print(f"{index + 1}. {girl.name} - {girl.update_relationship()}")
-            return True
-        else:
-            print("咒力不足或本子中沒有妹子！")
-            return False
-
-    def get_mana(self):
-        return self.mana
 
 def main():
     player = Player()
