0bedef74aad3be61759edfebae1c089cc7c1ac02
aloneworker <aloneworker@gmail.com>
2024-01-08 13:37:50 +0800

    Update FolderScanner.py

--- FolderScanner.py
+++ FolderScanner.py
@@ -19,6 +19,7 @@ class FolderScanner:
         掃描指定資料夾中的檔案。
         """
         folder_path = os.path.join(self.base_path, self.folder_name)
+        print(folder_path)
         if os.path.exists(folder_path) and os.path.isdir(folder_path):
             self.files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
         else:
